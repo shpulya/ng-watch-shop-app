74e6a61823f08e1b2b5856ce2d66b03b72dabf68
Irina <iryna.brychak@qlearsite.com>
2019-06-27 18:30:04 +0300

    improve carousel on item detail page

--- src/app/core/components/main-page/main-page.component.ts
+++ src/app/core/components/main-page/main-page.component.ts
@@ -4,22 +4,11 @@ import { ITEMS_SERVICES } from '../../services/items-factory.service';
 import { ItemsService } from '../../services/items.service';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
-import { trigger, state, style, transition, animate, keyframes, query } from '@angular/animations';
 
 @Component({
     selector: 'app-main-page',
     templateUrl: './main-page.component.html',
-    styleUrls: ['./main-page.component.scss'],
-    animations: [
-        trigger('scrollAnimation', [
-            state('i', style({
-                left: '{{scroll}}px',
-            }), {params: {scroll: 0}}),
-            transition(':enter', [style({transform: 'translateX(-100%)'}), animate('0.5s 300ms ease-in')]),
-            transition(':leave', [
-                animate('0.3s ease-out', style({transform: 'translateX(100%)'}))
-            ])
-        ])]
+    styleUrls: ['./main-page.component.scss']
 })
 
 export class MainPageComponent implements OnInit, OnDestroy {
@@ -29,8 +18,6 @@ export class MainPageComponent implements OnInit, OnDestroy {
     public items: Array<IItem> = [];
 
     public state: number = 0;
-
-    public scroll: number = 0;
 
     public activeIndex: number = 0;
 
@@ -62,7 +49,7 @@ export class MainPageComponent implements OnInit, OnDestroy {
     }
 
     public rightScroll(): void {
-        this.activeIndex = (this.activeIndex > this.items.length - 1) ? 0 : this.activeIndex + 1;
+        this.activeIndex = (this.activeIndex > this.items.length - 2) ? 0 : this.activeIndex + 1;
     }
 
     public leftScroll(): void {
--- src/app/shared/components/item-detail/carousel/carousel.component.ts
+++ src/app/shared/components/item-detail/carousel/carousel.component.ts
@@ -1,10 +1,22 @@
 import { Component, Input, OnInit, TemplateRef } from '@angular/core';
 import { IItem } from '../../../../app.models';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
     selector: 'app-carousel',
     templateUrl: './carousel.component.html',
-    styleUrls: ['./carousel.component.scss']
+    styleUrls: ['./carousel.component.scss'],
+    animations: [
+        trigger('slide', [
+            transition(':enter', [
+                style({ opacity: 0 }),
+                animate('1s', style({ opacity: 1}))
+            ]),
+            transition(':leave', [
+                animate('1s', style({opacity: 0}))
+            ])
+        ])
+    ]
 })
 export class CarouselComponent implements OnInit {
 
@@ -22,11 +34,9 @@ export class CarouselComponent implements OnInit {
 
     public increaseActiveIndex(): void {
         this.activeIndex = (this.activeIndex < this.items.length - 1) ? this.activeIndex + 1 : 0;
-        console.log(this.activeIndex);
     }
 
     public reduceActiveIndex(): void {
         this.activeIndex = (this.activeIndex <= 0) ? this.items.length - 1 : this.activeIndex - 1;
-        console.log(this.activeIndex);
     }
 }
